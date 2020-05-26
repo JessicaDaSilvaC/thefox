1f3eab6d431783aa6dd0986e5948f01dd735215c
AlxandrV <a.vigneaud@gmail.com>
2020-05-26 14:24:44 +0200

    Add fonts media

---
+++ script.js
@@ -0,0 +1,15 @@
+const burger = document.querySelector('.burger');
+burger.addEventListener('click', () => {
+    burger.classList.toggle('active');
+})
+
+const navSlide = () => {
+    const slideBurger = document.querySelector('.burger');
+    const slidePages = document.querySelector('.nav-links');
+
+    slideBurger.addEventListener('click', () => {
+        slidePages.classList.toggle('nav-active');
+    });
+}
+
+navSlide();
